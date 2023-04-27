0ca93d390ea55a07241d663135b31398adcbe951
Eduarda Araujo <74570744+eduardaac@users.noreply.github.com>
2023-04-27 14:43:54 -0300

    Verificando a implementação da leftRotate

--- ArvAVL.cpp
+++ ArvAVL.cpp
@@ -1,149 +1,369 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <sstream>
-#include "ArvAVL.h"
+#include "../include/Red-Black-Tree.hpp"
 
 using namespace std;
 
-ArvAVL::ArvAVL()
-{
-    raiz = NULL;
+RedBlackTree::RedBlackTree() {
+    TNULL = new Node;
+    TNULL->color = 0;
+    TNULL->left = nullptr;
+    TNULL->right = nullptr;
+    root = TNULL;
+}
+  
+void RedBlackTree::initializeNULLNode(NodePtr node, NodePtr parent) {
+    node->data = 0;
+    node->parent = parent;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->color = 0;
 }
 
-ArvAVL::~ArvAVL()
-{
-    
+// Preorder
+void RedBlackTree::preOrderHelper(NodePtr node) {
+  if (node != TNULL) {
+    cout << node->data << " ";
+    preOrderHelper(node->left);
+    preOrderHelper(node->right);
+  }
 }
 
-void ArvAVL::insere(int x)
-{
-    raiz = auxInsere(raiz, x);
+// Inorder
+void RedBlackTree::inOrderHelper(NodePtr node) {
+  if (node != TNULL) {
+    inOrderHelper(node->left);
+    cout << node->data << " ";
+    inOrderHelper(node->right);
+  }
 }
 
-NoArv* ArvAVL::auxInsere(NoArv *p, int x)
-{
-    if(p == NULL)
-    {
-        p = new NoArv();
-        p->setInfo(x);
-        p->setEsq(NULL);
-        p->setDir(NULL);
+// Post order
+void RedBlackTree::postOrderHelper(NodePtr node) {
+  if (node != TNULL) {
+    postOrderHelper(node->left);
+    postOrderHelper(node->right);
+    cout << node->data << " ";
+  }
+}
+
+NodePtr RedBlackTree::searchTreeHelper(NodePtr node, int key) {
+  if (node == TNULL || key == node->data) {
+    return node;
+  }
+
+  if (key < node->data) {
+    return searchTreeHelper(node->left, key);
+  }
+  return searchTreeHelper(node->right, key);
+}
+
+// For balancing the tree after deletion
+void RedBlackTree::deleteFix(NodePtr x) {
+  NodePtr s;
+  while (x != root && x->color == 0) {
+    if (x == x->parent->left) {
+      s = x->parent->right;
+      if (s->color == 1) {
+        s->color = 0;
+        x->parent->color = 1;
+        leftRotate(x->parent);
+        s = x->parent->right;
+      }
+
+      if (s->left->color == 0 && s->right->color == 0) {
+        s->color = 1;
+        x = x->parent;
+      } else {
+        if (s->right->color == 0) {
+          s->left->color = 0;
+          s->color = 1;
+          rightRotate(s);
+          s = x->parent->right;
+        }
+
+        s->color = x->parent->color;
+        x->parent->color = 0;
+        s->right->color = 0;
+        leftRotate(x->parent);
+        x = root;
+      }
+    } else {
+      s = x->parent->left;
+      if (s->color == 1) {
+        s->color = 0;
+        x->parent->color = 1;
+        rightRotate(x->parent);
+        s = x->parent->left;
+      }
+
+      if (s->right->color == 0 && s->right->color == 0) {
+        s->color = 1;
+        x = x->parent;
+      } else {
+        if (s->left->color == 0) {
+          s->right->color = 0;
+          s->color = 1;
+          leftRotate(s);
+          s = x->parent->left;
+        }
+
+        s->color = x->parent->color;
+        x->parent->color = 0;
+        s->left->color = 0;
+        rightRotate(x->parent);
+        x = root;
+      }
     }
-    else
-    {
-        char direcao;
-        cout << x << " a esquerda (e) ou direita (d) de " << p->getInfo() << ": ";
-        cin >> direcao;
-        if(direcao == 'e' || direcao == 'E')
-            p->setEsq(auxInsere(p->getEsq(), x));
-        else
-            p->setDir(auxInsere(p->getDir(), x));
+  }
+  x->color = 0;
+}
 
-        int fator = altura(p->getDir()) - altura(p->getEsq());
-        NoArv *q = p->getDir();
-        NoArv *r = p->getEsq();
-        int fatorDir = altura(q->getDir()) - altura(q->getEsq());
-        int fatorEsq = altura(r->getDir()) - altura(r->getEsq());
+void RedBlackTree::rbTransplant(NodePtr u, NodePtr v) {
+  if (u->parent == nullptr) {
+    root = v;
+  } else if (u == u->parent->left) {
+    u->parent->left = v;
+  } else {
+    u->parent->right = v;
+  }
+  v->parent = u->parent;
+}
 
-        if(fator == 2 )
-        {
-            if(fatorDir == 1 || fatorDir == 0)
-            {
-                rotSEsq(p);
-            }
-            else if(fatorDir == -1)
-            {
-                rotDEsq(p);
-            }
-        }
-        else if(fator == -2)
-        {
-            if(fatorEsq == -1 || fatorEsq == 0)
-            {
-                rotSDir(p);
-            }
-            else if(fatorEsq == 1)
-            {
-                rotDDir(p);
-            }
-        }
+void RedBlackTree::deleteNodeHelper(NodePtr node, int key) {
+  NodePtr z = TNULL;
+  NodePtr x, y;
+  while (node != TNULL) {
+    if (node->data == key) {
+      z = node;
     }
-    return p;
-}
 
-void ArvAVL::rotSDir(NoArv *p)
-{
-    NoArv *q;
-    q = p->getEsq();
-    p->setEsq(q->getDir());
-    q->setDir(p);
-}
-
-void ArvAVL::rotSEsq(NoArv *p)
-{
-    NoArv *q;
-    q = p->getDir();
-    p->setDir(q->getEsq());
-    q->setEsq(p);
-
-}
-
-void ArvAVL::rotDDir(NoArv *p)
-{
-    NoArv *q;
-    NoArv *r;
-    q = p->getEsq();
-    r = q->getDir();
-    p->setEsq(r->getDir());
-    q->setDir(r->getEsq());
-    r->setDir(p);
-    r->setEsq(q);
-}
-
-void ArvAVL::rotDEsq(NoArv *p)
-{
-    NoArv *q = p->getDir();
-    NoArv *r = q->getEsq();
-    p->setDir(r->getEsq());
-    q->setEsq(r->getDir());
-    r->setEsq(p);
-    r->setDir(q);
-}
-
-void ArvAVL::preOrdem()
-{
-    cout << "Arvore Binaria em Pre-Ordem: ";
-    auxPreOrdem(raiz);
-}
-
-void ArvAVL::auxPreOrdem(NoArv *p)
-{
-    if (p != NULL)
-    {
-        cout << p->getInfo() << " ";
-        auxPreOrdem(p->getEsq());
-        auxPreOrdem(p->getDir());
+    if (node->data <= key) {
+      node = node->right;
+    } else {
+      node = node->left;
     }
+  }
+
+  if (z == TNULL) {
+    cout << "Key not found in the tree" << endl;
+    return;
+  }
+
+  y = z;
+  int y_original_color = y->color;
+  if (z->left == TNULL) {
+    x = z->right;
+    rbTransplant(z, z->right);
+  } else if (z->right == TNULL) {
+    x = z->left;
+    rbTransplant(z, z->left);
+  } else {
+    y = minimum(z->right);
+    y_original_color = y->color;
+    x = y->right;
+    if (y->parent == z) {
+      x->parent = y;
+    } else {
+      rbTransplant(y, y->right);
+      y->right = z->right;
+      y->right->parent = y;
+    }
+
+    rbTransplant(z, y);
+    y->left = z->left;
+    y->left->parent = y;
+    y->color = z->color;
+  }
+  delete z;
+  if (y_original_color == 0) {
+    deleteFix(x);
+  }
 }
 
-int ArvAVL::altura(NoArv *p)
-{
-     if(p == NULL)
-     {
-         return -1;
-     }
-     else{
-        int he = altura(p->getEsq());
-        int hd = altura(p->getDir());
+// For balancing the tree after insertion
+void RedBlackTree::insertFix(NodePtr k) {
+  NodePtr u;
+  while (k->parent->color == 1) {
+    if (k->parent == k->parent->parent->right) {
+      u = k->parent->parent->left;
+      if (u->color == 1) {
+        u->color = 0;
+        k->parent->color = 0;
+        k->parent->parent->color = 1;
+        k = k->parent->parent;
+      } else {
+        if (k == k->parent->left) {
+          k = k->parent;
+          rightRotate(k);
+        }
+        k->parent->color = 0;
+        k->parent->parent->color = 1;
+        leftRotate(k->parent->parent);
+      }
+    } else {
+      u = k->parent->parent->right;
 
-        if(he>hd)
-        {
-            return he + 1;
+      if (u->color == 1) {
+        u->color = 0;
+        k->parent->color = 0;
+        k->parent->parent->color = 1;
+        k = k->parent->parent;
+      } else {
+        if (k == k->parent->right) {
+          k = k->parent;
+          leftRotate(k);
         }
-        else
-        {
-            return hd + 1;
-        }
-     }
+        k->parent->color = 0;
+        k->parent->parent->color = 1;
+        rightRotate(k->parent->parent);
+      }
+    }
+    if (k == root) {
+      break;
+    }
+  }
+  root->color = 0;
+}
+
+void RedBlackTree::printHelper(NodePtr root, string indent, bool last) {
+  if (root != TNULL) {
+    cout << indent;
+    if (last) {
+      cout << "R----";
+      indent += "   ";
+    } else {
+      cout << "L----";
+      indent += "|  ";
+    }
+
+    string sColor = root->color ? "RED" : "BLACK";
+    cout << root->data << "(" << sColor << ")" << endl;
+    printHelper(root->left, indent, false);
+    printHelper(root->right, indent, true);
+  }
+}
+
+void RedBlackTree::preorder() {
+  preOrderHelper(this->root);
+}
+
+void RedBlackTree::inorder() {
+  inOrderHelper(this->root);
+}
+
+void RedBlackTree::postorder() {
+  postOrderHelper(this->root);
+}
+
+NodePtr RedBlackTree::searchTree(int k) {
+  return searchTreeHelper(this->root, k);
+}
+
+NodePtr RedBlackTree::minimum(NodePtr node) {
+  while (node->left != TNULL) {
+    node = node->left;
+  }
+  return node;
+}
+
+NodePtr RedBlackTree::maximum(NodePtr node) {
+  while (node->right != TNULL) {
+    node = node->right;
+  }
+  return node;
+}
+
+NodePtr RedBlackTree::successor(NodePtr x) {
+  if (x->right != TNULL) {
+    return minimum(x->right);
+  }
+
+  NodePtr y = x->parent;
+  while (y != TNULL && x == y->right) {
+    x = y;
+    y = y->parent;
+  }
+  return y;
+}
+
+NodePtr RedBlackTree::predecessor(NodePtr x) {
+  if (x->left != TNULL) {
+    return maximum(x->left);
+  }
+
+  NodePtr y = x->parent;
+  while (y != TNULL && x == y->left) {
+    x = y;
+    y = y->parent;
+  }
+
+  return y;
+}
+
+void RedBlackTree::leftRotate(NodePtr x) {
+    NodePtr y = x->right;
+    x->right = successor(y->left);
+    y->left = predecessor(x);
+}
+
+void RedBlackTree::rightRotate(NodePtr x) {
+    NodePtr y = x->left;
+    x->left = successor(y->right);
+    y->right = predecessor(x);
+}                
+
+// Inserting a node
+void RedBlackTree::insert(int key) {
+  NodePtr node = new Node;
+  node->parent = nullptr;
+  node->data = key;
+  node->left = TNULL;
+  node->right = TNULL;
+  node->color = 1; // red
+
+  NodePtr y = nullptr;
+  NodePtr x = this->root;
+
+  while (x != TNULL) {
+    y = x;
+    if (node->data < x->data) {
+      x = x->left;
+    } else {
+      x = x->right;
+    }
+  }
+
+  node->parent = y;
+  if (y == nullptr) {
+    root = node;
+  } else if (node->data < y->data) {
+    y->left = node;
+  } else {
+    y->right = node;
+  }
+
+  if (node->parent == nullptr) {
+    node->color = 0;
+    return;
+  }
+
+  if (node->parent->parent == nullptr) {
+    return;
+  }
+
+  insertFix(node);
+}
+
+NodePtr RedBlackTree::getRoot() {
+  return this->root;
+}
+
+void RedBlackTree::deleteNode(int data) {
+  deleteNodeHelper(this->root, data);
+}
+
+void RedBlackTree::printTree() {
+  if (root) {
+    printHelper(this->root, "", true);
+  }
 }
